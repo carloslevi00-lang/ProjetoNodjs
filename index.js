b814e539f560933d9425930fe8dc6f9bea02e151
bruno <ghavy6barbosa7187@gmail.com>
2026-04-28 17:36:10 -0300

    Atualização de Dados.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", function(req,res){
-    res.send("<h1>Bem vindo ao meu site novo!</h1>");
+    res.send("<h1> Bem-vindo ao CineNode - VERSÃO ATUALIZADA!</h1>");
 })
 
 app.get("/produtos", function(req,res){
